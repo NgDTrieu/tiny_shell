49446b2ffb0e73c6728955d020b2f1015264a01a
NgDTrieu <nguyenductrieu.toank30chl@gmail.com>
2024-06-05 22:09:42 +0700

    Lưu tạm

--- main.c
+++ main.c
@@ -21,10 +21,16 @@
 #include <ctype.h>
 #include <time.h>
 #include <signal.h>
+#include <limits.h>
 
 #define BUFSIZE 1024
 #define TOK_BUFSIZE 64
 #define TOK_DELIM " \t\r\n\a"
+#define MAN_DIR "./man/"
+#define PATH_MAX 4096
+
+// Biến toàn cục để lưu thư mục chứa chương trình
+char home_directory[PATH_MAX];
 
 /*
   Khai báo các lệnh
@@ -36,6 +42,7 @@ int hthsh_runsh(char **args);
 int hthsh_lsdir(char **args);
 int hthsh_showtime(char **args);
 int hthsh_runapp(char **args);
+int hthsh_man(char **args); 
 
 /*
   Danh sách các lệnh được xây dựng
@@ -47,7 +54,8 @@ char *builtin_str[] = {
   "runsh",
   "lsdir",
   "time",
-  "runapp"
+  "runapp",
+  "man"
 };
 
 int (*builtin_func[]) (char **) = {
@@ -58,6 +66,7 @@ int (*builtin_func[]) (char **) = {
   &hthsh_lsdir,
   &hthsh_showtime,
   &hthsh_runapp,
+  &hthsh_man,
 };
 
 int hthsh_num_builtins() {
@@ -70,10 +79,14 @@ int hthsh_num_builtins() {
 int hthsh_cd(char **args)
 {
   if (args[1] == NULL) {
-    fprintf(stderr, "hthsh: Cần tham số cho lệnh \"cd\"\n");
+    fprintf(stderr, "🤧 Cần tham số cho lệnh \"cd\"\n");
+  } else if (strcmp(args[1], "~") == 0) {
+    if (chdir(home_directory) != 0) {
+      perror("🙈");
+    }
   } else {
     if (chdir(args[1]) != 0) {
-      perror("hthsh");
+      perror("🙈");
     }
   }
   return 1;
@@ -96,12 +109,13 @@ int hthsh_help(char **args)
 
 int hthsh_exit(char **args)
 {
+  printf("Goodbye 🙋 See you soon 💕 \n");
   return 0;
 }
 
 int hthsh_runsh(char **args) {
   if (args[1] == NULL) {
-    fprintf(stderr, "hthsh: Cần tham số cho lệnh \"runsh\"\n");
+    fprintf(stderr, "🤧 Cần tham số cho lệnh \"runsh\"\n");
     return 1;
   }
 
@@ -135,9 +149,10 @@ int hthsh_lsdir(char **args)
     while ((dir = readdir(d)) != NULL) {
       if (strcmp(dir->d_name, ".") == 0 || strcmp(dir->d_name, "..") == 0)
         continue;
-      printf("%s\n", dir->d_name);
+      printf("%s  ", dir->d_name);
     }
     closedir(d);
+    printf("\n");
   }
   return 1;
 }
@@ -174,7 +189,7 @@ int hthsh_showtime(char **args)
 
     time (&rawtime);
     timeinfo = localtime (&rawtime);
-    printf ("Thời gian hiện tại là: %s", asctime(timeinfo));
+    printf ("⏱️ Thời gian hiện tại là: %s", asctime(timeinfo));
 
     return 1;
 }
@@ -182,7 +197,7 @@ int hthsh_showtime(char **args)
 int hthsh_runapp(char **args)
 {
     if (args[1] == NULL) {
-        fprintf(stderr, "hthsh: Cần tham số cho lệnh \"runapp\"\n");
+        fprintf(stderr, "🤧 Cần tham số cho lệnh \"runapp\"\n");
         return 1;
     }
 
@@ -200,8 +215,34 @@ int hthsh_runapp(char **args)
         // Tiến trình cha
         int status;
         waitpid(pid, &status, 0); // Chờ tiến trình con kết thúc
-        printf("Ứng dụng %s đã kết thúc\n", args[1]);
+        printf("Ứng dụng %s đã kết thúc 👻\n", args[1]);
     }
+    return 1;
+}
+
+int hthsh_man(char **args) {
+    if (args[1] == NULL) {
+        fprintf(stderr, "🤧 Cần tham số cho lệnh \"man\"\n");
+        return 1;
+    }
+
+    char filepath[BUFSIZE];
+    snprintf(filepath, sizeof(filepath), "%s%s.txt", MAN_DIR, args[1]);
+
+    FILE *file = fopen(filepath, "r");
+    if (!file) {
+        fprintf(stderr, "🙈 Không tìm thấy trang hướng dẫn cho lệnh \"%s\"\n", args[1]);
+        return 1;
+    }
+
+    char *line = NULL;
+    size_t len = 0;
+    while (getline(&line, &len, file) != -1) {
+        printf("%s", line);
+    }
+
+    free(line);
+    fclose(file);
     return 1;
 }
 
@@ -254,7 +295,7 @@ char** hthsh_split_line(char* line) {
       bufsize += TOK_BUFSIZE;
       tokens = realloc(tokens, bufsize * sizeof(char*));
       if (!tokens) {
-        fprintf(stderr, "hthsh: lỗi cấp phát\n");
+        fprintf(stderr, "hthsh:  lỗi cấp phát\n");
         exit(EXIT_FAILURE);
       }
     }
@@ -288,7 +329,7 @@ void hthsh_loop() {
   int status;
 
   do {
-    printf("> ");
+    printf("(👉ﾟヮﾟ)👉 ");
     line = hthsh_read_line();
     args = hthsh_split_line(line);
     status = hthsh_execute(args);
@@ -298,7 +339,23 @@ void hthsh_loop() {
   } while (status);
 }
 
+void print_welcome_message() {
+    printf("*******************************************************************************\n");
+    printf("  @file         main.c\n");
+    printf("  @author       Pham Quang Hung, Nguyen Duc Trieu, Tran Vuong Hoang\n");
+    printf("  @date         March 2024\n");
+    printf("  @brief        HTHSH (HoangTrieuHung SHell)\n");
+    printf("*******************************************************************************\n");
+    printf("\n");
+    printf("🚀 Welcome to HTHSH (HoangTrieuHung SHell)! 🚀\n\n");
+}
+
 int main(int argc, char** argv) {
+  if (getcwd(home_directory, sizeof(home_directory)) == NULL) {
+    perror("getcwd() error");
+    return 1;
+  }
+  print_welcome_message();
   hthsh_loop();
   return EXIT_SUCCESS;
 }
